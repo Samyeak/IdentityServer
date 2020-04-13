54abcb23e13cf08f4d9b98d499c56648a1a413fd
samyeak <samyeakmhj@protonmail.com>
2020-04-13 22:59:55 +0545

    Client Authentication Process with Identity Server and Accessing Api Resource with Access Token

--- quickstart/src/Client/Program.cs
+++ quickstart/src/Client/Program.cs
@@ -1,12 +1,72 @@
-﻿using System;
+﻿using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            DiscoveryDocumentResponse disco = await DiscoverEndpoints();
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            TokenResponse tokenResponse = await RequestToken(disco.TokenEndpoint);
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+            Console.WriteLine(tokenResponse.Json);
+
+            GetResource(tokenResponse.AccessToken);
+
+            Console.ReadKey();
+        }
+
+        static private async Task<DiscoveryDocumentResponse> DiscoverEndpoints()
+        {
+            var identityUrl = "http://localhost:5000";
+            var client = new HttpClient();
+            return await client.GetDiscoveryDocumentAsync(identityUrl);
+        }
+
+        static private async Task<TokenResponse> RequestToken(string tokenEndpoint)
+        {
+            var client = new HttpClient();
+            return await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = tokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            });
+        }
+
+        static private async void GetResource(string accessToken)
+        {
+            string url = "http://localhost:5001/identity";
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(accessToken);
+
+            HttpResponseMessage response = await apiClient.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
         }
     }
 }
